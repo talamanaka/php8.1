b477811ea0af91f9bdba600162de993420682bbb
talamanaka <36954803+talamanaka@users.noreply.github.com>
2022-05-24 04:14:29 +0000

    Update getresults.php

--- src/requesthandler/getresults.php
+++ src/requesthandler/getresults.php
@@ -8,6 +8,8 @@ $finale_name = $php_id."_".$php_key.".pdf";
 $file = $finale_name;
 $filename = $finale_name;
 // Header content type
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 header('Content-type: application/pdf');
   
 header('Content-Disposition: inline; filename="' . $filename . '"');
