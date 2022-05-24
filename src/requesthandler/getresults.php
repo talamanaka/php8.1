7bfd446daa5ca62f0a25ddebe63c670878e124a4
talamanaka <36954803+talamanaka@users.noreply.github.com>
2022-05-24 05:26:57 +0000

    Update getresults.php

--- src/requesthandler/getresults.php
+++ src/requesthandler/getresults.php
@@ -15,10 +15,23 @@ function redirect($url)
 }
 
 
+function getting_pdf($url)
+{
+    header('Content-type: application/octet-stream');
+    header('Content-Disposition: inline; filename="hat_007.pdf"');
+    header('Content-Disposition: inline; filename="' . $url . '"');
+
+    readfile($url); 
+    #header('Location: '.$url);
+    #exit();
+}
+
+
 
   
 if (file_exists($file_pointer)) 
 {
+    getting_pdf($file_pointer);
     redirect($file_pointer); 
     #echo "The file $file_pointer exists";
     #header('Location:"' . $filename . '"');
