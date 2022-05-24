0e9d96e4b5035b7cb97840f232efeb0e6053d285
talamanaka <36954803+talamanaka@users.noreply.github.com>
2022-05-24 04:50:39 +0000

    Update getresults.php

--- src/requesthandler/getresults.php
+++ src/requesthandler/getresults.php
@@ -28,9 +28,9 @@ else
 {
     echo "The file $file_pointer does
                              not exists";
-   header('Location: index.html');
+   header('Location: ../index.html');
 }
-header('Content-type: application/octet-stream');
-header('Content-Disposition: inline; filename="hat_007.pdf"');
-readfile('hat_007.pdf'); 
+#header('Content-type: application/octet-stream');
+#header('Content-Disposition: inline; filename="hat_007.pdf"');
+#readfile('hat_007.pdf'); 
 ?>
