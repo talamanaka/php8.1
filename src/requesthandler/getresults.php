5819ed48730c4477df5bfccc9f271d79ccf5aca3
talamanaka <36954803+talamanaka@users.noreply.github.com>
2022-05-24 04:28:12 +0000

    Update getresults.php

--- src/requesthandler/getresults.php
+++ src/requesthandler/getresults.php
@@ -1,4 +1,18 @@
 <?php
+
+
+// checking whether file exists or not
+$file_pointer = '/user01/work/gfg.txt';
+  
+if (file_exists($file_pointer)) 
+{
+    echo "The file $file_pointer exists";
+}
+else 
+{
+    echo "The file $file_pointer does
+                             not exists";
+}
 header('Content-type: application/octet-stream');
 header('Content-Disposition: inline; filename="hat_007.pdf"');
 readfile('hat_007.pdf'); 
